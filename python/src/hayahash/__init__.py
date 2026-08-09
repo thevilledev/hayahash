fa93e24036a75e9447a0f1cf97f6452b36cff1ee
Ville Vesilehto <ville@vesilehto.fi>
2026-08-09 15:19:37 +0300

    chore: release v0.5.0
    Signed-off-by: Ville Vesilehto <ville@vesilehto.fi>

--- python/src/hayahash/__init__.py
+++ python/src/hayahash/__init__.py
@@ -9,6 +9,6 @@ from hayahash._hayahash import hayahash64, hayahash128
 try:
     __version__ = version("hayahash")
 except PackageNotFoundError:  # pragma: no cover - editable/uninstalled tree
-    __version__ = "0.4.6"
+    __version__ = "0.5.0"
 
 __all__ = ["__version__", "hayahash64", "hayahash128"]
--- swift/Package.swift
+++ swift/Package.swift
@@ -1,5 +1,5 @@
 // swift-tools-version: 5.9
-// hayahash-version: 0.4.6
+// hayahash-version: 0.5.0
 //
 // The hayahash-version line is the lockstep algorithm version shared with
 // every other language port; scripts/bump-version.sh and the release
